2d19b48b56c3c36e82a58bed98619c1584fa9d67
Dave Pagurek <dave@davepagurek.com>
2015-02-18 09:53:09 -0500

    Add linux version

--- README.md
+++ README.md
@@ -4,11 +4,18 @@ A joke I'm playing on my sister where I remotely change her wallpaper to picture
 See the past wallpapers and web interface here: http://davepagurek.com/stuff/wallpaper
 
 <h2>Client setup</h2>
+<h3>Windows</h3>
 <ol>
   <li>Run <code>python setup.py py2exe</code> to create an exe to store on the client (using Python 3)</li>
   <li>Edit <code>client/wallpaper.vbs</code> to reflect the correct path to the generated exe</li>
   <li>Edit <code>client/identity.txt</code> to give a unique string to each client listening for wallpapers</li>
   <li>Open the Windows Task Scheduler and import <code>client/update_wallpaper.xml</code> to create scheduled tasks to update wallpapers. Edit the task's Action to the location you have <code>wallpaper.vbs</code> stored.</li>
+</ol>
+
+<h3>Linux</h3>
+<ol>
+  <li>Run <code>crontab -e</code> to edit  cron jobs</li>
+  <li>Add the line <code>0 * * * * python3 /home/username/path/to/file/wallpaper.py</code> to run the script every hour</li>
 </ol>
 
 <h2>Server Setup</h2>
---
+++ client/wallpaper_linux.py
@@ -0,0 +1,69 @@
+from gi.repository import Gtk, Gio
+import os
+import urllib.request
+import random
+import string
+import re
+
+#change wallpaper even if it's not new
+FORCE = True
+
+SCHEMA = 'org.gnome.desktop.background'
+KEY = 'picture-uri'
+gsettings = Gio.Settings.new(SCHEMA)
+
+def random_string(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+#Get latest wallpaper from the web
+latest = urllib.request.urlopen("http://saripagurek.com/wallpaper/latest.txt")
+num = int(latest.readline().decode('utf-8') )
+url = latest.readline().decode('utf-8')
+
+#Isolate the filename from all the previous directories
+r = re.search(r'[\/\\]*([a-zA-Z0-9-_ ]*\.[a-zA-Z]+)$', url)
+filename = r.group(1)
+
+filepath = ""
+
+#Check if the website's file is newer than ours
+update = False
+if os.path.isfile("current.txt"):
+	current = open("current.txt")
+	old_num = int(current.readline() )
+	filepath = current.readline()
+	
+	if num > old_num:
+		update = True
+
+#If we have none so far, download the newest
+else:
+	update = True
+
+#Apply new wallpaper
+if update:
+
+	#Hide the file in an obscure directory
+	localpath = "/home/steve/Downloads/" + random_string()  + "/" + random_string() + "/" + random_string()
+	if not os.path.exists(localpath):
+		os.makedirs(localpath)
+	urllib.request.urlretrieve("http://saripagurek.com/wallpaper/" + url, localpath + "/" + filename)
+
+	#Mark the number
+	current = open("current.txt", "w")
+	current.write(str(num) + "\n")
+	current.write(os.path.join("file://" + localpath + "/", filename))
+
+	#Apply wallpaper
+	gsettings.set_string(KEY, os.path.join("file://" + localpath + "/", filename))
+
+
+	identity = open("identity.txt")
+	id = identity.readline()
+	read = urllib.request.urlopen("http://www.davepagurek.com/stuff/wallpaper/read.pl?id=" + id)
+
+	
+elif (FORCE and len(filepath) > 0):
+	#Apply wallpaper
+	gsettings.set_string(KEY, filepath)
+
