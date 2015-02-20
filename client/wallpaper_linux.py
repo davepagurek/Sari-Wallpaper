3e198139ae4b9957af0c1470af45d4518c8a6b32
Dave Pagurek <dave@davepagurek.com>
2015-02-20 07:25:50 -0500

    Fix for when there are characters that need to be urlencoded

--- client/wallpaper_linux.py
+++ client/wallpaper_linux.py
@@ -1,4 +1,4 @@
-from gi.repository import Gtk, Gio
+from gi.repository import Gio
 import os
 import urllib.request
 import random
@@ -13,12 +13,12 @@ KEY = 'picture-uri'
 gsettings = Gio.Settings.new(SCHEMA)
 
 def random_string(size=6, chars=string.ascii_uppercase + string.digits):
-	return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(chars) for _ in range(size))
 
 #Get latest wallpaper from the web
-latest = urllib.request.urlopen("http://saripagurek.com/wallpaper/latest.txt")
+latest = urllib.request.urlopen("http://davepagurek.com/stuff/wallpaper/latest.txt")
 num = int(latest.readline().decode('utf-8') )
-url = latest.readline().decode('utf-8')
+url = latest.readline().decode('utf-8').rstrip()
 
 #Isolate the filename from all the previous directories
 r = re.search(r'[\/\\]*([a-zA-Z0-9-_ ]*\.[a-zA-Z]+)$', url)
@@ -29,41 +29,41 @@ filepath = ""
 #Check if the website's file is newer than ours
 update = False
 if os.path.isfile("current.txt"):
-	current = open("current.txt")
-	old_num = int(current.readline() )
-	filepath = current.readline()
-	
-	if num > old_num:
-		update = True
+    current = open("current.txt")
+    old_num = int(current.readline() )
+    filepath = current.readline()
+
+    if num > old_num:
+        update = True
 
 #If we have none so far, download the newest
 else:
-	update = True
+    update = True
 
 #Apply new wallpaper
 if update:
 
-	#Hide the file in an obscure directory
-	localpath = "/home/steve/Downloads/" + random_string()  + "/" + random_string() + "/" + random_string()
-	if not os.path.exists(localpath):
-		os.makedirs(localpath)
-	urllib.request.urlretrieve("http://saripagurek.com/wallpaper/" + url, localpath + "/" + filename)
+    #Hide the file in an obscure directory
+    localpath = "/home/steve/Downloads/" + random_string()  + "/" + random_string() + "/" + random_string()
+    if not os.path.exists(localpath):
+        os.makedirs(localpath)
+        urllib.request.urlretrieve("http://" + urllib.parse.quote("davepagurek.com/stuff/wallpaper/" + url), localpath + "/" + filename)
 
-	#Mark the number
-	current = open("current.txt", "w")
-	current.write(str(num) + "\n")
-	current.write(os.path.join("file://" + localpath + "/", filename))
+    #Mark the number
+    current = open("current.txt", "w")
+    current.write(str(num) + "\n")
+    current.write(os.path.join("file://" + localpath + "/", filename))
 
-	#Apply wallpaper
-	gsettings.set_string(KEY, os.path.join("file://" + localpath + "/", filename))
+    #Apply wallpaper
+    gsettings.set_string(KEY, os.path.join("file://" + localpath + "/", filename))
 
 
-	identity = open("identity.txt")
-	id = identity.readline()
-	read = urllib.request.urlopen("http://www.davepagurek.com/stuff/wallpaper/read.pl?id=" + id)
+    identity = open("identity.txt")
+    id = identity.readline()
+    read = urllib.request.urlopen("http://www.davepagurek.com/stuff/wallpaper/read.pl?id=" + id)
 
-	
+
 elif (FORCE and len(filepath) > 0):
-	#Apply wallpaper
-	gsettings.set_string(KEY, filepath)
+    #Apply wallpaper
+    gsettings.set_string(KEY, filepath)
 
