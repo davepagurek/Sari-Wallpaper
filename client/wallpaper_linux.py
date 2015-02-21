baad31628768ac00d39f0ec29a4fe68230f4f7ab
Dave Pagurek <dave@davepagurek.com>
2015-02-21 09:43:23 -0500

    Fix cron job environment variables

--- client/wallpaper_linux.py
+++ client/wallpaper_linux.py
@@ -8,17 +8,14 @@ import re
 #change wallpaper even if it's not new
 FORCE = True
 
-SCHEMA = 'org.gnome.desktop.background'
-KEY = 'picture-uri'
-gsettings = Gio.Settings.new(SCHEMA)
 
 def random_string(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+	return ''.join(random.choice(chars) for _ in range(size))
 
 #Get latest wallpaper from the web
-latest = urllib.request.urlopen("http://davepagurek.com/stuff/wallpaper/latest.txt")
+latest = urllib.request.urlopen("http://saripagurek.com/wallpaper/latest.txt")
 num = int(latest.readline().decode('utf-8') )
-url = latest.readline().decode('utf-8').rstrip()
+url = latest.readline().decode('utf-8')
 
 #Isolate the filename from all the previous directories
 r = re.search(r'[\/\\]*([a-zA-Z0-9-_ ]*\.[a-zA-Z]+)$', url)
@@ -29,41 +26,36 @@ filepath = ""
 #Check if the website's file is newer than ours
 update = False
 if os.path.isfile("current.txt"):
-    current = open("current.txt")
-    old_num = int(current.readline() )
-    filepath = current.readline()
-
-    if num > old_num:
-        update = True
+	current = open("current.txt")
+	old_num = int(current.readline() )
+	filepath = current.readline()
+	
+	if num > old_num:
+		update = True
 
 #If we have none so far, download the newest
 else:
-    update = True
+	update = True
+
 
 #Apply new wallpaper
 if update:
 
-    #Hide the file in an obscure directory
-    localpath = "/home/steve/Downloads/" + random_string()  + "/" + random_string() + "/" + random_string()
-    if not os.path.exists(localpath):
-        os.makedirs(localpath)
-        urllib.request.urlretrieve("http://" + urllib.parse.quote("davepagurek.com/stuff/wallpaper/" + url), localpath + "/" + filename)
+	#Hide the file in an obscure directory
+	localpath = "/home/steve/Downloads/" + random_string()  + "/" + random_string() + "/" + random_string()
+	if not os.path.exists(localpath):
+		os.makedirs(localpath)
+	urllib.request.urlretrieve("http://" + urllib.parse.quote("saripagurek.com/" + url.rstrip()), localpath + "/" + filename)
 
-    #Mark the number
-    current = open("current.txt", "w")
-    current.write(str(num) + "\n")
-    current.write(os.path.join("file://" + localpath + "/", filename))
+	#Mark the number
+	current = open("current.txt", "w")
+	current.write(str(num) + "\n")
+	current.write(os.path.join("file://" + localpath + "/", filename))
 
-    #Apply wallpaper
-    gsettings.set_string(KEY, os.path.join("file://" + localpath + "/", filename))
+	#Apply wallpaper
+	os.system("DISPLAY=:0 GSETTINGS_BACKEND=dconf DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-2YdDw7dn6T gsettings set org.gnome.desktop.background picture-uri '%s'" % os.path.join("file://" + localpath + "/", filename))
 
-
-    identity = open("identity.txt")
-    id = identity.readline()
-    read = urllib.request.urlopen("http://www.davepagurek.com/stuff/wallpaper/read.pl?id=" + id)
-
-
+	
 elif (FORCE and len(filepath) > 0):
-    #Apply wallpaper
-    gsettings.set_string(KEY, filepath)
-
+	#Apply wallpaper
+	os.system("DISPLAY=:0 GSETTINGS_BACKEND=dconf DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-2YdDw7dn6T gsettings set org.gnome.desktop.background picture-uri '%s'" % filepath)
